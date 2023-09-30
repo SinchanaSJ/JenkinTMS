20e5c82542109f6af28f1cdef15c12f3f01c8ee0
SinchanaSJ <sinchanasinchhu@gmail.com>
2023-09-30 13:51:56 +0530

    Sample commit

--- src/test/java/com/tmsystem/SampleTest.java
+++ src/test/java/com/tmsystem/SampleTest.java
@@ -8,4 +8,10 @@ public void sampleTest() {
 	System.out.println("--- Sample Test 1---");
 	System.out.println("--- Sample Test 2---");
 }
+
+@Test
+public void sampleTest2() {
+	System.out.println("--- Sample Test 12---");
+	System.out.println("--- Sample Test 12---");
+}
 }
